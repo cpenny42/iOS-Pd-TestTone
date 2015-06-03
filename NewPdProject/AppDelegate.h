91fda0917554c10f24cd5470378a67cf6530f596
Chris Penny <cpennny@gmail.com>
2015-06-03 18:39:48 -0400

    Initial Commit

---
+++ NewPdProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewPdProject
+//
+//  Created by Chris Penny on 6/3/15.
+//  Copyright (c) 2015 Tufts University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewPdProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewPdProject
+//
+//  Created by Chris Penny on 6/3/15.
+//  Copyright (c) 2015 Tufts University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
